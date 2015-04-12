7436bc8a065a80b4e67c3db50cb03d1913d50b62
Jose Francisco Rives Lirola <jose@digio.es>
2015-04-12 02:28:57 +0200

    new scripts for linux and osx

--- init.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-%~d0
-cd %~p0
-bash vagrant init digio/webdev
-echo Vagrant Webserver initialized, please run start.cmd for download and running the box
--- start.cmd
+++ start.cmd
@@ -1,6 +1,4 @@
 @echo off
-%~d0
-cd %~p0
 bash vagrant up
 bash vagrant ssh
 bash vagrant suspend
