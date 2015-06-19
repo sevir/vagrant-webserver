da267ccd220d9fbd54414dd6cb71a299b24fc42b
Jose Francisco Rives Lirola <jose@digio.es>
2015-06-20 01:01:46 +0200

    some fixes

--- tools/scripts/package_box_prepare.sh
+++ tools/scripts/package_box_prepare.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 echo "apt-get clean..."
 sudo apt-get clean
+sudo apt-get autoclean
+sudo apt-get autoremove
 echo "zeroed freespace..."
 sudo dd if=/dev/zero of=/EMPTY bs=1M
 sudo rm -rf /EMPTY
 echo "removing logs..."
-sudo rm -rf /var/log/*
+sudo find /var/log -type f -name "*.gz" -delete
+sudo find /var/log -type f -name "*.log" -delete
 echo "copying ssh keys"
-cat /vagrant/tools/insecure_private_key.pub > ~/.ssh/authorized_keys
-cat /vagrant/tools/vagrant.pub >> ~/.ssh/authorized_keys
+cat /vagrant/tools/ssh-keys/insecure_private_key.pub > ~/.ssh/authorized_keys
+cat /vagrant/tools/ssh-keys/vagrant.pub >> ~/.ssh/authorized_keys
 echo "cleaning history"
 cat /dev/null > ~/.bash_history && history -c
 sudo cat /dev/null > /root/.bash_history && sudo history -c
--- tools/shares/default.rb
+++ tools/shares/default.rb
@@ -1,2 +1,2 @@
 # Please add files with synced folders
-# config.vm.synced_folder "c:/www", "/www", type: "nfs"
+# config.vm.synced_folder "c:/www", "/www", type: "nfs",mount_options:["nolock,vers=3,udp,noatime,fsc"]
